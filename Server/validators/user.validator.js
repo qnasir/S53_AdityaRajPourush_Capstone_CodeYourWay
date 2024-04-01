19ab5f1e22831cb39519a66103f4748727f1c74e
AdityaPourush <adityapourush11@outlook.com>
2024-04-01 11:33:14 +0530

    validator

--- Server/validators/user.validator.js
+++ Server/validators/user.validator.js
@@ -1,8 +1,18 @@
 const z = require("zod");
 
 const userSchema = z.object({
-    username: z.string().trim().toLowerCase(),
-    email: z.string().trim().toLowerCase().email()
-})
+  username: z.string().trim().toLowerCase(),
+  email: z.string().trim().toLowerCase().email(),
+  password: z.string().optional(),
+  role: z.enum(["admin", "user"]).default("user"),
+  fullname: z.string().trim(),
+  profileImage: z
+    .string()
+    .default(
+      "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
+    ),
+  refreshToken: z.string().optional(),
+  questionsDone: z.array(z.string().cuid()).optional(),
+});
 
-module.exports = userSchema
+module.exports = userSchema;
