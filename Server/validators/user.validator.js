dffb018ef458a09048197b900c106e841724d441
AdityaPourush <adityapourush11@outlook.com>
2024-04-01 15:49:57 +0530

    jwt methods for accesstoken and refresh token

--- Server/validators/user.validator.js
+++ Server/validators/user.validator.js
@@ -1,47 +1,41 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const userValidator = Joi.object({
   username: Joi.string()
     .min(3)
-    .max(30)
+    .max(20)
     .required()
     .lowercase()
     .trim()
     .pattern(/^[a-zA-Z0-9_]+$/)
     .messages({
-      'string.min': 'Username must be at least 3 characters long',
-      'string.max': 'Username must be at most 30 characters long',
-      'string.pattern.base': 'Username can only contain alphanumeric characters and underscores',
+      "string.min": "Username must be at least 3 characters long",
+      "string.max": "Username must be at most 20 characters long",
+      "string.pattern.base":
+        "Username can only contain alphanumeric characters and underscores only",
     }),
-  email: Joi.string()
-    .email()
-    .required()
-    .lowercase()
-    .trim()
-    .messages({
-      'string.empty': 'Email is a compulsary field',
-      'string.email': 'Please enter a valid email address',
-    }),
+  email: Joi.string().email().required().lowercase().trim().messages({
+    "string.empty": "Email is a compulsary field",
+    "string.email": "Please enter a valid email address",
+  }),
   password: Joi.string(),
-  role: Joi.string()
-    .valid('admin', 'user')
-    .default('user'),
-  fullname: Joi.string()
-    .required()
-    .trim()
-    .messages({
-      'string.empty': 'Full name is required',
-    }),
+  role: Joi.string().valid("admin", "user").default("user"),
+  fullname: Joi.string().required().trim().min(3)
+  .max(30).messages({
+    "string.empty": "Full name is required",
+    "string.min": "Full name must be at least 3 characters long",
+    "string.max": "Full name must be at most 30 characters long",
+  }),
   profileImage: Joi.string()
     .uri()
-    .default('https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration\\_268834-538.jpg'),
+    .default(
+      "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration\\_268834-538.jpg"
+    ),
   refreshToken: Joi.string(),
-  questionsDone: Joi.array()
-    .items(Joi.string().hex().length(24))
-    .messages({
-      'string.length': 'Invalid ObjectId format',
-      'string.hex': 'Invalid ObjectId format',
-    }),
+  questionsDone: Joi.array().items(Joi.string().hex().length(24)).messages({
+    "string.length": "Invalid ObjectId format",
+    "string.hex": "Invalid ObjectId format",
+  }),
 });
 
 module.exports = userValidator;
