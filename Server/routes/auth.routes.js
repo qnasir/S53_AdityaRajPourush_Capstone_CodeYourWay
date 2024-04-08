57c294610e5a5b94c58bbfb6058527e3819cb876
AdityaPourush <adityapourush11@outlook.com>
2024-04-08 17:51:39 +0530

    log-out controller and jwt-verification middleware

--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -3,9 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const {signUpUser, logInUser, logOutUser} = require("../controllers/auth.controller");
+const { verifyJWTToken } = require("../middleware/auth.middleware");
 
 router.post('/signup', signUpUser);
 router.post('/login', logInUser);
-router.post('/logout', logOutUser);
+
+// Secured by JWT Token verification
+router.post('/logout',verifyJWTToken, logOutUser);
 
 module.exports = router
