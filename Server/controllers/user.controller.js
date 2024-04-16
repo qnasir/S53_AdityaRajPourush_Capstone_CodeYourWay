b6aafca1004e28bf39ded2622c2b031c47b9a85a
AdityaPourush <adityapourush11@outlook.com>
2024-04-16 17:38:52 +0530

    profile-image upload and delete controller

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model")
-const {ApiError} = require("../utils/ApiError")
+const {ApiError} = require("../utils/ApiError");
+const { uploadOnCloudinary } = require("../utils/cloudinary");
 const userValidator = require("../validators/user.validator");
 
 // @desc get user profile
@@ -72,10 +73,51 @@ const updatePassword = async (req, res, next) => {
     }
 }
 
+const updateProfileImage = async (req, res, next) => {
+    try {
+        const imageLocalPath = req.file?.path;
 
+        if(!imageLocalPath){
+            const err = new ApiError(400, "Profile image is required");
+            return next(err);
+        }
+
+        const image = await uploadOnCloudinary(imageLocalPath);
+
+        if(!image.url){
+            const err = new ApiError(500, "Cannot upload profile image");
+            return next(err);
+        }
+
+        const user = await User.findByIdAndUpdate(req.user._id, {
+            $set: {
+                profileImage: image.url
+            }
+        }, {new: true}
+    ).select("-password -refreshToken");
+
+    return res.status(200).json({user, message: "Profile image updated successfully"})
+
+    } catch (error) {
+        const err = new ApiError(500, error?.message || "Cannot update profile image");
+        next(err);
+    }
+}
+
+const deleteProfile = async(req, res, next)=> {
+    try {
+        await User.findByIdAndDelete(req.user._id);
+        res.status(200).json({message: "Profile deleted successfully"});
+    } catch (error) {
+        const err = new ApiError(500, error?.message || "Cannot delete Your Profile");
+        next(err);
+    }
+}
 
 module.exports = {
     getUserProfile,
     updateUserProfile,
-    updatePassword
+    updatePassword,
+    updateProfileImage,
+    deleteProfile
 }
