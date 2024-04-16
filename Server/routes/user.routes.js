55eede3f2243fde2e60a7647a3b19b485e7fc1d6
AdityaPourush <adityapourush11@outlook.com>
2024-04-16 15:07:52 +0530

    controllers for profile

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getUserProfile, updateUserProfile } = require("../controllers/user.controller");
+const { getUserProfile, updateUserProfile, updatePassword } = require("../controllers/user.controller");
 const {verifyJWTToken} = require("../middleware/auth.middleware");
 
 
 //Secured Routes
 router.get("/profile", verifyJWTToken, getUserProfile);
 router.patch('/update-profile', verifyJWTToken, updateUserProfile);
-router.patch('/update-password', verifyJWTToken, updateUserProfile);
+router.patch('/update-password', verifyJWTToken, updatePassword);
 
 module.exports = router
