b2d35ec9538b28ac99743e43a3f50707365d382b
AdityaPourush <adityapourush11@outlook.com>
2024-05-18 10:20:14 +0530

    responsive navbar

--- Client/src/components/Navbar_1.jsx
+++ Client/src/components/Navbar_1.jsx
@@ -1,54 +1,115 @@
 import React, { useContext } from "react";
-import { Button } from "./ui/button";
-import { ModeToggle } from "./mode-toggle";
-
-import { useTheme } from "@/components/context/theme-provider";
 import { Link } from "react-router-dom";
 import { AuthContext } from "./context/authContext";
+import { useTheme } from "@/components/context/theme-provider";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./mode-toggle";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 const Navbar_1 = () => {
   const { theme } = useTheme();
-
   const navbarBgColor = theme === "dark" ? "bg-fuchsia-800" : "bg-violet-400";
   const navbarTextColor =
     theme === "dark" ? "text-slate-300" : "text-slate-800";
-
   const { isLoggedIn, username, logout } = useContext(AuthContext);
 
   return (
     <div
-      className={`navbar flex items-center p-[1vh] ${navbarBgColor} ${navbarTextColor}`}
+      className={`navbar flex flex-col items-center p-[0.5] sm:p-[0.8vh] w-full ${navbarBgColor} ${navbarTextColor}`}
     >
-      <div className="logo w-[20vw] flex justify-center items-center">
-        <h1>CodeYourWay</h1>
-      </div>
-      <div className="flex items-center justify-between w-[80vw] px-[1vw]">
-        <div className="nav-links">
-          <ul className="flex justify-evenly w-[20vw]">
-            <li>Problems</li>
-            <li>Discuss</li>
-          </ul>
+      <div className="w-full max-w-7xl m-auto flex flex-col items-center sm:flex-row sm:justify-between">
+        <div className="logo w-full flex justify-between sm:justify-center items-center sm:w-auto mb-4 sm:mb-0 px-[1vw]">
+          <h1>CodeYourWay</h1>
+          <Drawer>
+            <DrawerTrigger asChild>
+              <Button variant="ghost" size="icon" className="sm:hidden">
+                <HamburgerMenuIcon />
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <div className="flex justify-between w-full sm:w-auto">
+                  <ModeToggle />
+                  <div>
+                    {isLoggedIn ? (
+                      <div className="flex justify-between">
+                        <Button variant="outline">{username}</Button>
+                        <Button
+                          variant="ghost"
+                          onClick={logout}
+                          className="ml-2"
+                        >
+                          Logout
+                        </Button>
+                      </div>
+                    ) : (
+                      <div className="flex justify-between">
+                        <Link to={"/signup"}>
+                          <Button variant="ghost">Register</Button>
+                        </Link>
+                        <Link to={"/login"}>
+                          <Button variant="ghost" className="ml-2">
+                            Login
+                          </Button>
+                        </Link>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </DrawerHeader>
+              <DrawerFooter>
+                <div className="nav-links">
+                  <ul>
+                    <li>Problems</li>
+                    <li>Discuss</li>
+                    <li>Playground</li>
+                  </ul>
+                </div>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
         </div>
-        <div className="flex justify-around w-[20vw]">
-          <ModeToggle />
-          <div className="flex justify-around">
-            {isLoggedIn ? (
-              <>
-                <Button variant="outline">{username}</Button>
-                <Button variant="ghost" onClick={logout}>
-                  Logout
-                </Button>
-              </>
-            ) : (
-              <>
-                <Link to={"/signup"}>
-                  <Button variant="ghost">Register</Button>
-                </Link>
-                <Link to={"/login"}>
-                  <Button variant="ghost">Login</Button>
-                </Link>
-              </>
-            )}
+        <div className="hidden sm:flex sm:flex-row sm:items-center">
+          <div className="nav-links">
+            <ul className="flex justify-evenly w-[40vw]">
+              <li>Problems</li>
+              <li>Discuss</li>
+              <li>Playground</li>
+            </ul>
+          </div>
+          <div className="flex justify-between w-full sm:w-auto">
+            <ModeToggle />
+            <div>
+              {isLoggedIn ? (
+                <div className="flex justify-between">
+                  <Button variant="outline">{username}</Button>
+                  <Button variant="ghost" onClick={logout} className="ml-2">
+                    Logout
+                  </Button>
+                </div>
+              ) : (
+                <div className="flex justify-between">
+                  <Link to={"/signup"}>
+                    <Button variant="ghost">Register</Button>
+                  </Link>
+                  <Link to={"/login"}>
+                    <Button variant="ghost" className="ml-2">
+                      Login
+                    </Button>
+                  </Link>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
--- Client/src/pages/LandingPage.jsx
+++ Client/src/pages/LandingPage.jsx
@@ -1,10 +1,29 @@
 import Navbar_1 from '@/components/Navbar_1'
-import React from 'react'
+import { AuthContext } from '@/components/context/authContext'
+import { Button } from '@/components/ui/button'
+import React, { useContext } from 'react'
 
 const LandingPage = () => {
+  const {username, isLoggedIn} = useContext(AuthContext)
+
   return (
     <div>
-        <Navbar_1/>
+      <Navbar_1/>
+      <div className='w-full px-4 py-8'>
+        <div className='mb-8'>
+          <h1 className='text-2xl font-bold'>Hi {isLoggedIn ? username : "User"}</h1>
+          <h1 className='text-3xl font-bold'>Let's Code Your Way to Success!</h1>
+        </div>
+        <div className='flex flex-col sm:flex-row flex-wrap justify-between'>
+          <div className='w-full sm:w-[48%] border my-4 rounded px-4 py-6'>
+            <Button variant="outline">Online Compiler</Button>
+          </div>
+          <div className='w-full sm:w-[48%] border my-4 rounded px-4 py-6'>
+            <h2 className='text-xl font-bold mb-2'>Profile</h2>
+            {/* Add profile content here */}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
